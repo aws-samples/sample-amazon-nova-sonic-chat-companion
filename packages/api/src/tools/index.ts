804b1e7892a0b994b385fde538009b2f00bde8fb
Massimiliano Angelino <angmas@amazon.com>
2025-08-05 10:29:31 +0100

    chore: init

---
+++ packages/api/src/tools/index.ts
@@ -0,0 +1,20 @@
+import { DateTool } from "./dateTool";
+import { DateDiffTool } from "./dateDiffTool";
+import { TimeTool } from "./timeTool";
+import { WeatherTool } from "./weatherTool";
+import { WeatherForecastTool } from "./weatherForecastTool";
+import { CalculatorTool } from "./calculatorTool";
+import { WebSearchTool } from "./webSearchTool";
+import { LatestNewsTool } from "./latestNewsTool";
+import { WebArticleFetchTool } from "./webArticleFetchTool";
+export default {
+  DateTool,
+  DateDiffTool,
+  TimeTool,
+  WeatherTool,
+  CalculatorTool,
+  WebSearchTool,
+  WebArticleFetchTool,
+  LatestNewsTool,
+  WeatherForecastTool,
+};
